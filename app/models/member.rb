4405578d1ffeb85f7b552a6f9d626a84d0a0c4e9
Darwin Groskleg <darwingroskleg@gmail.com>
2015-09-19 23:34:59 -0400

    Adds methods for name and photo.

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,12 @@
 class Member < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  def name
+    [first_name, last_name].join ' '
+  end
+
+  def photo
+    "members/#{[first_name, last_name].join '_'}.jpg"
+  end
 end
