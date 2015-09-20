78b21f2e2976d1907b66ae0488891b325f30409c
Darwin Groskleg <darwingroskleg@gmail.com>
2015-09-19 22:39:36 -0400

    Adds tests for member model

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -5,4 +5,20 @@ RSpec.describe Member, type: :model do
     it { should validate_presence_of :first_name }
     it { should validate_presence_of :last_name }
   end
+
+  describe '.name' do
+    it 'concatenates first name before last name' do
+      member = create :member
+
+      expect(member.name).to eq 'John Yakabuski'
+    end
+  end
+
+  describe '.photo' do
+    it 'returns the photo filepath for the member' do
+      member = create :member
+
+      expect(member.photo).to eq 'members/John_Yakabuski.jpg'
+    end
+  end
 end
