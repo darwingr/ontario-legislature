55e4a2c3d42ba4afb99a76ee10ecdbce26670da1
Darwin Groskleg <darwingroskleg@gmail.com>
2016-10-30 18:48:02 -0400

    Adds class methods on Member needed for guesses

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,6 +2,16 @@ class Member < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
 
+  def self.all_names(excluded_name)
+    names = Member.pluck(:first_name, :last_name).map { |m| m.join(' ') }
+    names.delete excluded_name
+    names.uniq
+  end
+
+  def self.random_member
+    order("RANDOM()").limit(1)
+  end
+
   def name
     [first_name, last_name].join ' '
   end
--- spec/factories/members.rb
+++ spec/factories/members.rb
@@ -7,4 +7,15 @@ FactoryGirl.define do
     riding 'Renfrew—Nipissing—Pembroke'
   end
 
+  factory :liberal_member, parent: :member do
+    first_name 'Kathleen'
+    last_name 'Wynne'
+    riding 'Don Valley West'
+  end
+
+  factory :ndp_member, parent: :member do
+    first_name 'Andrea'
+    last_name 'Horwath'
+    riding 'Hamilton Centre'
+  end
 end
