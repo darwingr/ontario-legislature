9285a700f6a0ab310c224ed4b9a996d87e98a659
Darwin Groskleg <darwingroskleg@gmail.com>
2016-10-30 15:20:08 -0400

    Removes controller tests on assigns ivar

--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -37,34 +37,15 @@ RSpec.describe MembersController, type: :controller do
   let(:valid_session) { {} }
 
   describe "GET #index" do
-    it "assigns all members as @members" do
-      member = Member.create! valid_attributes
-      get :index, {}, valid_session
-      expect(assigns(:members)).to eq([member])
-    end
   end
 
   describe "GET #show" do
-    it "assigns the requested member as @member" do
-      member = Member.create! valid_attributes
-      get :show, {:id => member.to_param}, valid_session
-      expect(assigns(:member)).to eq(member)
-    end
   end
 
   describe "GET #new" do
-    it "assigns a new member as @member" do
-      get :new, {}, valid_session
-      expect(assigns(:member)).to be_a_new(Member)
-    end
   end
 
   describe "GET #edit" do
-    it "assigns the requested member as @member" do
-      member = Member.create! valid_attributes
-      get :edit, {:id => member.to_param}, valid_session
-      expect(assigns(:member)).to eq(member)
-    end
   end
 
   describe "POST #create" do
@@ -75,12 +56,6 @@ RSpec.describe MembersController, type: :controller do
         }.to change(Member, :count).by(1)
       end
 
-      it "assigns a newly created member as @member" do
-        post :create, {:member => valid_attributes}, valid_session
-        expect(assigns(:member)).to be_a(Member)
-        expect(assigns(:member)).to be_persisted
-      end
-
       it "redirects to the created member" do
         post :create, {:member => valid_attributes}, valid_session
         expect(response).to redirect_to(Member.last)
@@ -88,11 +63,6 @@ RSpec.describe MembersController, type: :controller do
     end
 
     context "with invalid params" do
-      it "assigns a newly created but unsaved member as @member" do
-        post :create, {:member => invalid_attributes}, valid_session
-        expect(assigns(:member)).to be_a_new(Member)
-      end
-
       it "re-renders the 'new' template" do
         post :create, {:member => invalid_attributes}, valid_session
         expect(response).to render_template("new")
@@ -113,12 +83,6 @@ RSpec.describe MembersController, type: :controller do
         skip("Add assertions for updated state")
       end
 
-      it "assigns the requested member as @member" do
-        member = Member.create! valid_attributes
-        put :update, {:id => member.to_param, :member => valid_attributes}, valid_session
-        expect(assigns(:member)).to eq(member)
-      end
-
       it "redirects to the member" do
         member = Member.create! valid_attributes
         put :update, {:id => member.to_param, :member => valid_attributes}, valid_session
@@ -127,12 +91,6 @@ RSpec.describe MembersController, type: :controller do
     end
 
     context "with invalid params" do
-      it "assigns the member as @member" do
-        member = Member.create! valid_attributes
-        put :update, {:id => member.to_param, :member => invalid_attributes}, valid_session
-        expect(assigns(:member)).to eq(member)
-      end
-
       it "re-renders the 'edit' template" do
         member = Member.create! valid_attributes
         put :update, {:id => member.to_param, :member => invalid_attributes}, valid_session
