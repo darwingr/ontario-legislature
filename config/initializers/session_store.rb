c64db622b26c687f10a7e5537fc88790ebd3e855
Darwin Groskleg <darwingroskleg@gmail.com>
2016-10-29 23:34:52 -0400

    Updates to rails 5, with related app configuration changes

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_ontario-legislature_session'
+Rails.application.config.session_store :cookie_store, key: '_ontario_legislature_session'
